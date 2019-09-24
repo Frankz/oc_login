78c96ca93f0a50dae978548c2ec66cb36b9ce3f8
Frankz <franciscojavier.jerez@gmail.com>
2019-09-24 10:25:43 -0300

    add more details to the readme, and some fixes for read files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # oc-login
-Add custom message description about your oc-login app.
+
+Just a menu for choose a menu to login in OCP
+
+## Params
+
+oc_login wait for a param "$1", this param must be a csv file with the next structure
+
+```
+hostname1.com,port,protocol
+hostname2.com,port,protocol
+...
+hostnameN.com,port,protocol
+```
+
+## FAQ
+
+dependecies?
+yes, of course!, gnu bc and oc (the OCP client)
--- oc-login.sh
+++ oc-login.sh
@@ -5,15 +5,73 @@ function oc-login () {
   # @Param $2 = OCP_PORT OCP server api port 
   # @Param $3 = OCP_USER OCP client username
   # @Param $4 = OCP_PASS OCP client password
+  if [[ -z $1 ]]; then
+    echo "Must include a param with a file to read"
+    return 1
+    exit 
+  fi
+  
+  if [[ -e $1 ]]; then
+    :
+   else
+    echo "The param isn't a file"
+    return 1
+    exit 
+  fi
+  fileContent=$(_readFile $1)
+  echo -e $fileContent
+  OCP_HOST=$(_hostMenu $fileContent)
+  echo "You has been selected \"$OCP_HOST\""
+
   echo "OCP_HOST $OCP_HOST"
-  OCP_HOST=${1:-localhost}
-  OCP_PORT=${2:-8443}
-  OCP_USER=${3:-myuser}
-  OCP_PASS=${4:-mypass}
+ # OCP_HOST=${1:-localhost}
+  OCP_PORT=${2:-443}
+  #OCP_USER=${3:-myuser}
+  read -p "Insert username: " OCP_USER
+  #OCP_PASS=${4:-mypass}
+  read -s -p "Insert password:" OCP_PASS
   #echo ${OC_HOST}
-  echo oc login ${OCP_HOST}:${OCP_PORT} --username=${OCP_USER} --password=${OCP_PASS}
+  oc login ${OCP_HOST}:${OCP_PORT} --username=${OCP_USER} --password=${OCP_PASS}
   
 }
+
+function _hostMenu () {
+  read -p "Choose an option:" OPTION
+  OPTION=$(echo -e $fileContent | grep ^$OPTION | cut -d"," -f1 | cut -d" " -f3)
+  echo $OPTION
+}
+
+function _readFile() {
+  # @Param $1 filename with path location
+  #items_list="$(cat $1)"
+  #echo -e $items_list
+  FILENAME=$1
+  declare -i I=1
+
+  FILENAME=$(_sanitizeFile $FILENAME)
+  declare -i LAST_LINE=$(echo "$(cat $FILENAME | wc -l) + 1" | bc)
+
+  if [[ "$@" == "--details" ]]; then
+    for LINE in $(cat ${FILENAME})
+    do
+      TEXT+="$((I++)) | ${LINE}\n"
+    done
+  else
+    for LINE in $(cat ${FILENAME})
+    do
+      #if [[ $LAST_LINE -eq ]]\
+      TEXT+="$((I++))  - ${LINE}\n"
+    done
+  fi
+  echo ${TEXT}
+}
+
+function _sanitizeFile() {
+  # This functions remove windows carraige return and normalize all for *unix
+  # @Param $1 File with path
+  echo -e $1  | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g'
+}
+
 
 ## detect if being sourced and
 ## export if so else execute
